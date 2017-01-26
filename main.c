6f60f537448b63ff725f470453474a3387f725d4
Nick <nicholashannani@yahoo.com>
2017-01-26 02:50:37 -0500

    Changes to be committed: 	new file:   main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    This program takes in a string as input, and outputs a string where
+    the tabs are replaced by a specified number of spaces. The first argument
+    to the program should be the number of spaces to replace each tab with.
+    The second argument should be the input string.
+
+    Example: Detab 4 "Detab this    file"
+*/
+
+int main(int argc, char** argv))
+{
+    int s_num; // number of spaces
+    char* input_string, output_string;
+
+    s_num = to_i(
+
+                printf("Hello world!\n");
+                return 0;
+}
