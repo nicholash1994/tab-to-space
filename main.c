d1bbd6dfcaacabe000410dcf534d7d7b9e5d776d
Nick <nicholashannani@yahoo.com>
2017-01-26 05:01:30 -0500

    Changes to be committed: 	modified:   main.c

--- main.c
+++ main.c
@@ -14,52 +14,92 @@
     Example: tab-to-space 4 "Detab this    string"
 */
 
+void print_usage_err();
+void detab_string(const char* input_string, const int s_num);
+void detab_file(const char* filename, const int s_num);
+
 int main(int argc, char** argv) {
-    int i, j;
-    int s_num; // number of spaces
-    int len; // length of input string
-    char* input_string;
-    char* output_string;
+	int s_num; // number of spaces
 
-    if (argc == 1) {
-        printf("Usage: ./tab-to-space n string\n");
-        printf("Example: ./tab-to-space n \"This is a string\t with a tab.\"\n");
-        return -1;
-    }
-    else if (argc == 2) {
-        printf("Only 1 argument given! Defaulting to 4 spaces per tab!\n");
-        s_num = 4;
-        input_string = argv[1];
-    }
-    else {
-        input_string = argv[2];
-        s_num = atoi(argv[1]);
-        if (s_num > 8) {
-            printf("Error: Number of tabs cannot exceed 8!\n");
-            return -1;
-        }
-    }
-    if (s_num <= 0) {
-        printf("Error: the number of spaces must be positive!\n");
-        return -1;
-    }
+    //  these if statements process the command-line arguments
+	if (argc == 1 || argc == 2) {
+		print_usage_err();
+		return -1;
+	}
+	s_num = atoi(argv[1]);
+	if (s_num <= 0 || s_num > 8) {
+		printf("Error: The number of spaces per tab must be between 1 and 8.\n");
+		return -1;
+	}
+	if (strcmp("-f", argv[2]) == 0)
+		detab_file(argv[3], s_num);
+	else
+		detab_string(argv[2], s_num);
 
-    output_string = (char*)malloc((s_num*len+1)*sizeof(char));
+	return 0;
+}
 
-    j = 0;
-    for (i = 0; i < len; i++) {
-        if (input_string[i] == '\t') {
-            memset(output_string+j, ' ', s_num*sizeof(char));
-            j += s_num;
-        }
-        else {
-            output_string[j] = input_string[i];
-            j++;
-        }
-    }
-    output_string[j] = '\0';
+void print_usage_err() {
+	printf("Usage: ./tab-to-space n string\n");
+	printf("  OR   ./tab-to-space n -f filename\n\n");
+	printf("Example: ./tab-to-space n \"This is a string\t with a tab.\"\n");
+}
 
-    printf("%s\n", output_string);
+void detab_string(const char* input_string, const int s_num) {
+	char* output_string;
+	int len, i, j;
 
-    return 0;
+	len = strlen(input_string);
+	output_string = (char*)malloc((s_num*len+1)*sizeof(char));
+
+	j = 0;
+	for (i = 0; i < len; i++) {
+		if (input_string[i] == '\t') {
+			memset(output_string+j, ' ', s_num*sizeof(char));
+			j += s_num;
+		}
+		else {
+			output_string[j] = input_string[i];
+			j++;
+		}
+	}
+	output_string[j] = '\0';
+
+	printf("%s\n", output_string);
+
+	free(output_string);
+}
+
+void detab_file(const char* filename, const int s_num) {
+	FILE* file;
+	char in_buffer[32];
+    //char* out_buffer = (char*)malloc(s_num*32*sizeof(char));
+	int i, j, bytes;
+
+	if ((file = fopen(filename, "r")) == NULL) {
+		printf("Error: file couldn't be opened!\n");
+		exit(-1);
+	}
+
+	j = 0;
+	while (1) {
+		bytes = fread(in_buffer, sizeof(char), 32, file);
+		if (ferror(file)) {
+			printf("Error while reading file!\n");
+			exit(-1);
+		}
+		for (i = 0; i < bytes; i++)
+			if (in_buffer[i] == '\t')
+				for (j = 0; j < s_num; j++)
+					putchar(' ');
+			else
+				putchar(in_buffer[i]);
+		if (feof(file))
+			break;
+	}
+
+	if (fclose(file)) {
+		printf("Error while attempting to close file!\n");
+		exit(-1);
+	}
 }
